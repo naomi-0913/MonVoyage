42010051c502d40a6d1eab3dfdc08076c69d1688
naomi-0913 <upworld409019@gmail.com>
2019-07-16 17:19:16 +0900

    delete-and-create-memoriesController

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :memories
   devise_for :users
-  root 'memory#index'
-  resources :memory
+  root 'memories#index'
+  resources :memories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
