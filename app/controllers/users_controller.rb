ad0a9102bf13a99104afb2d632da5625c9ad6e5c
naomi-0913 <upworld409019@gmail.com>
2019-09-01 15:20:20 +0900

    update.user-show.html-scss

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@ class UsersController < ApplicationController
   def show 
     @user =User.find(params[:id])
     @memory =Memory.where(user_id: @user.id)
+    @likes = Like.where(user_id: params[:id])
   end
 end
